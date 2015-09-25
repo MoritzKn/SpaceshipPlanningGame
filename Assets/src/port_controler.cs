fcd576764d16ab22824ae84cd8c16ea73f1dbdba
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2015-09-25 20:49:23 +0200

    Add selection code

--- Assets/src/port_controler.cs
+++ Assets/src/port_controler.cs
@@ -3,13 +3,32 @@ using System.Collections;
 
 public class port_controler : MonoBehaviour {
 
+	public Material mat_selected;
+	public Material standard;
+	public bool selected;
+	Renderer rend;
+	Collider coll;
+
 	// Use this for initialization
 	void Start () {
-	
+		 rend = gameObject.GetComponent<Renderer>();
+		 coll = gameObject.GetComponent<Collider>();
 	}
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (Input.GetButtonDown("Fire1")) {
+			RaycastHit hitinfo;
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+			if (Physics.Raycast(ray,out hitinfo) && hitinfo.collider==coll ){
+				rend.material = mat_selected;
+				selected=true;
+
+			}
+			else{
+				rend.material = standard;
+				selected=false;
+			}
+		}
 	}
 }
