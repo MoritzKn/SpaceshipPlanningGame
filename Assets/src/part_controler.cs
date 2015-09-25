bd5bd8476fd180e8e887f9d6b94e2e601cb8f728
Moritz Kneilmann <moritz.kneilmann@gmx.de>
2015-09-25 23:55:24 +0200

    bla

--- Assets/src/part_controler.cs
+++ Assets/src/part_controler.cs
@@ -4,7 +4,7 @@ using System.Collections;
 public class part_controler : MonoBehaviour {
     [SerializeField] private string name;
     [SerializeField] private int size = 1;
-
+    
     public string getName(){
         return name;
     }
