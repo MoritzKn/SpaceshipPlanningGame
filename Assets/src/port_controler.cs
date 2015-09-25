c3316b40d1b564a60c784c53719bddf060228374
Ninja-Koala <felix.potthast@student.uni-siegen.de>
2015-09-25 23:50:52 +0200

    Delete unneded publics

--- Assets/src/port_controler.cs
+++ Assets/src/port_controler.cs
@@ -2,9 +2,7 @@
 using System.Collections;
 
 public class port_controler : MonoBehaviour {
-
-	public Material mat_selected;
-	public Material standard;
+	
 	public bool selected;
 	public bool selectable;
 	public Renderer rend;
